e7e82ea77c434b618353d0170a596e6efd4a8b96
EliasFarah2 <eliasfarah776@gmail.com>
2024-11-17 14:27:05 +0200

    test3

--- src/SharedData.java
+++ src/SharedData.java
@@ -1,17 +1,13 @@
 import java.util.ArrayList;
 
 public class SharedData 
-{
+{//dff
 	private ArrayList<Integer> array;
 	private boolean [] winArray;
 	private boolean flag;
 	private final int b;
-	
-<<<<<<< HEAD
-	public SharedData(int[] array, int b) {
-=======
+
 	public SharedData(ArrayList<Integer> array, int b) {
->>>>>>> branch 'master' of https://github.com/khalil212002/ex2.git
 		
 		this.array = array;
 		this.b = b;
